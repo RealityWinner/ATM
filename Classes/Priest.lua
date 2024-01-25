9fc6e0a40a3872622930b5a0bbff43a0a685c0c2
RealityWinner <31143064+RealityWinner@users.noreply.github.com>
2024-01-24 21:56:16 -0500

    Priest spells

--- Classes/Priest.lua
+++ Classes/Priest.lua
@@ -29,286 +29,157 @@ function prototype:shadowAffinity()
     return self.shadowMod
 end
 
-prototype.spells = {
-    --Martyrdom, Inner Focus, Inspiration, Lightwell, Silence cause no threat
-    --DISCIPLINE SPELLS
-    ["Dispel Magic"] = {
-        ranks = {
-            --2x if used on enemy
-            [988] = 36, --CONFIRMED 
-            [527] = 18, --CONFIRMED 
-        },
-        handler = ATM.Dispel,
-    },
 
-    ["Divine Spirit"] = {
-        ranks = {
-            [27841] =  60,
-            [14819] =  50,
-            [14818] =  40,
-            [14752] =  30,
-        },
-        type = "BUFF",
-    },
+--Dispel Magic
+s[527] = {threat=18,handler=ATM.Dispel}
+s[988] = {threat=36,handler=ATM.Dispel}
 
-    --MULTIPLY BY NUMBER OF BUFFED PLAYERS
-    ["Prayer of Spirit"] = {
-        ranks = {
-            [27681] =  60,
-        },
-        type = "BUFF",
-    },
+--Divine Spirit
+s[27841] = {onBuff=true,threat=60}
+s[14819] = {onBuff=true,threat=50}
+s[14818] = {onBuff=true,threat=40}
+s[14752] = {onBuff=true,threat=30}
 
-    ["Inner Fire"] = {
-        ranks = {
-            [10952] =  60, --CONFIRMED
-            [10951] =  50, --CONFIRMED
-             [1006] =  40, --CONFIRMED
-              [602] =  30, --CONFIRMED
-             [7128] =  20, --CONFIRMED
-              [588] =  10, --CONFIRMED           
-        },
-        type = "BUFF",
-    },
+--Prayer of Spirit
+s[27681] = {onBuff=true,threat=60}
 
-    ["Levitate"] = {
-        ranks = {
-            [1706] = 34, --CONFIRMED
-        },
-        type = "BUFF",
-    },
-    
-    ["Power Infusion"] = {
-        ranks = {
-            [10060] = 40, --CONFIRMED
-        },
-        type = "BUFF",
-    },
+--Inner Fire
+s[10952] = {onBuff=true,threat=60}
+s[10951] = {onBuff=true,threat=50}
+s[1006]  = {onBuff=true,threat=40}
+s[602]   = {onBuff=true,threat=30}
+s[7128]  = {onBuff=true,threat=20}
+s[588]   = {onBuff=true,threat=10}
 
-    ["Power Word: Fortitude"] = {
-        ranks = {
-            [10938] = 60, --CONFIRMED
-            [10937] = 48, --CONFIRMED
-             [2791] = 36, --CONFIRMED
-             [1245] = 24, --CONFIRMED
-             [1244] = 12, --CONFIRMED
-             [1243] =  1, --CONFIRMED
-        },
-        type = "BUFF",
-    },
+--Levitate
+s[1706]  = {onBuff=true,threat=34}
 
-    ["Power Word: Shield"] = {
-        ranks = {
-            [10901] = 99,   --CONFIRMED
-            [10900] = 99,
-            [10899] = 99,
-            [10898] = 99,
-             [6066] = 99,   --CONFIRMED
-             [6065] = 76,   --CONFIRMED
-             [3747] = 53.5, --CONFIRMED
-              [600] = 35,   --CONFIRMED
-              [592] = 20.5, --CONFIRMED
-               [17] = 11,   --CONFIRMED
-        },
-        type = "BUFF",
-    },
+--Power Infusion
+s[10060] = {onBuff=true,threat=40}
 
-    --MULTIPLY BY NUMBER OF BUFFED PLAYERS
-    ["Prayer of Fortitude"] = {
-        ranks = {
-            [21564] = 60, 
-            [21562] = 48, 
-        },
-        type = "BUFF",
-    },
+--Power Word: Fortitude
+s[10938] = {onBuff=true,threat=60}
+s[10937] = {onBuff=true,threat=48}
+s[2791]  = {onBuff=true,threat=36}
+s[1245]  = {onBuff=true,threat=24}
+s[1244]  = {onBuff=true,threat=12}
+s[1243]  = {onBuff=true,threat=1}
 
-    ["Shackle Undead"] = {
-        ranks = {
-            [10955] = 120, --CONFIRMED
-             [9485] =  80, --CONFIRMED
-             [9484] =  40, --CONFIRMED
-        },
-        type = "CC",
-    },
-    
-    ["Abolish Disease"] = {
-        ranks = {
-            [552] = 32, --CONFIRMED
-        },
-        type = "BUFF",
-    },
+--Power Word: Shield
+s[10901] = {onBuff=true,threat=99}
+s[10900] = {onBuff=true,threat=99}
+s[10899] = {onBuff=true,threat=99}
+s[10898] = {onBuff=true,threat=99}
+s[6066]  = {onBuff=true,threat=99}
+s[6065]  = {onBuff=true,threat=76}
+s[3747]  = {onBuff=true,threat=53.5}
+s[600]   = {onBuff=true,threat=35}
+s[592]   = {onBuff=true,threat=20.5}
+s[17]    = {onBuff=true,threat=11}
 
-    ["Cure Disease"] = {
-        ranks = {
-            [528] = 14, --CONFIRMED
-        },
-        type = "CAST"
-    },
+--Prayer of Fortitude
+s[21564] = {onBuff=true,threat=60}
+s[21562] = {onBuff=true,threat=48}
 
-    ["Fear Ward"] = {
-        ranks = {
-            [6346] = 20, --CONFIRMED
-        },
-        type = "BUFF",
-    },
-    
-    --HOLY SPELLS
-    ["Holy Nova"] = {
-        ranks = {
-            27801,
-            27800,
-            27799,
-            15431,
-            15430,
-            15237,
-        },
-        ignored = true,
-    },
+--Shackle Undead
+s[10955] = {isCC=true,onDebuff=true,threat=120}
+s[9485]  = {isCC=true,onDebuff=true,threat=80}
+s[9484]  = {isCC=true,onDebuff=true,threat=40}
 
-    ["Lightwell Renew"] = {
-        ranks = {
-            27874,
-            27873,
-            7001,
-        },
-        ignored = true,
-    },
+--Abolish Disease
+s[552]   = {onBuff=true,threat=32}
 
-    --SHADOW SPELLS
-    ["Mind Soothe"] = {
-        ranks = {
-            10953,
-            8192,
-            453,
-        },
-        ignored = true,
-    },
-    ["Mind Vision"] = {
-        ranks = {
-            10909,
-            2096,
-        },
-        ignored = true,
-    },
+--Cure Disease
+s[528]   = {threat=14,handler=ATM.Dispel}
 
-    ["Fade"] = {
-        ranks = {
-            [10942] = ATM.Player.levelBasedThreat(-820, -3, 60, 70),
-            [10941] = ATM.Player.levelBasedThreat(-620, -3, 50, 60),
-             [9592] = ATM.Player.levelBasedThreat(-440, -3, 40, 50),
-             [9579] = ATM.Player.levelBasedThreat(-285, -3, 30, 40),
-             [9578] = ATM.Player.levelBasedThreat(-155, -3, 20, 30),
-              [586] = ATM.Player.levelBasedThreat( -55, -3,  8, 18),
-        },
-        handler = ATM.TemporaryThreat
-    },
+--Fear Ward
+s[6346]  = {onBuff=true,threat=20}
 
-    ["Mind Blast"] = {
-        ranks = {
-            [10947] = 540, --CONFIRMED
-            [10946] = 460, --CONFIRMED
-            [10945] = 380, --CONFIRMED
-             [8106] = 303, --CONFIRMED
-             [8105] = 236, --CONFIRMED
-             [8104] = 180, --CONFIRMED
-             [8103] = 121, --CONFIRMED
-             [8102] =  77, --CONFIRMED
-             [8092] =  40, --CONFIRMED
-        },
-        threatMod = prototype.shadowAffinity,
-        type = "DAMAGE",
-    },
+--Holy Nova
+s[27801] = {ignored=true}
+s[27800] = {ignored=true}
+s[27799] = {ignored=true}
+s[15431] = {ignored=true}
+s[15430] = {ignored=true}
+s[15237] = {ignored=true}
 
-    --APPLY ONLY ONCE ON DEBUFF
-    ["Mind Flay"] = {
-        ranks = {
-            [18807] = 120, --CONFIRMED
-            [17314] = 104, --CONFIRMED
-            [17313] =  88, --CONFIRMED
-            [17312] =  72, --CONFIRMED
-            [17311] =  56, --CONFIRMED
-            [15407] =  40, --CONFIRMED
-        },
-        threatMod = prototype.shadowAffinity,
-        type = "DEBUFF",
-    },
+--Lightwell Renew
+s[27874] = {ignored=true}
+s[27873] = {ignored=true}
+s[7001]  = {ignored=true}
 
-    ["Psychic Scream"] = {
-        ranks = {
-            [10890] = 112, --CONFIRMED
-            [10888] =  84, --CONFIRMED
-             [8124] =  56, --CONFIRMED
-             [8122] =  28, --CONFIRMED
-        },
-        threatMod = prototype.shadowAffinity,
-        type = "CC",
-    },
+--Mind Soothe
+s[10953] = {ignored=true}
+s[8192]  = {ignored=true}
+s[453]   = {ignored=true}
 
-    ["Shadow Protection"] = {
-        ranks = {
-            [10958] = 56, --CONFIRMED
-            [10957] = 42, --CONFIRMED
-              [976] = 30, --CONFIRMED
-        },
-        threatMod = prototype.shadowAffinity,
-        type = "BUFF",
-    },
+--Mind Vision
+s[10909] = {ignored=true}
+s[2096]  = {ignored=true}
 
-    --MULTIPLY BY NUMBER OF BUFFED PLAYERS
-    ["Prayer of Shadow Protection"] = {
-        ranks = {
-            [27683] = 56,
-        },
-        threatMod = prototype.shadowAffinity,
-        type = "BUFF",
-    },
+--Fade
+s[10942] = {threat=ATM.Player.levelBasedThreat(-820, -3, 60, 70),handler=ATM.TemporaryThreat}
+s[10941] = {threat=ATM.Player.levelBasedThreat(-620, -3, 50, 60),handler=ATM.TemporaryThreat}
+s[9592]  = {threat=ATM.Player.levelBasedThreat(-440, -3, 40, 50),handler=ATM.TemporaryThreat}
+s[9579]  = {threat=ATM.Player.levelBasedThreat(-285, -3, 30, 40),handler=ATM.TemporaryThreat}
+s[9578]  = {threat=ATM.Player.levelBasedThreat(-155, -3, 20, 30),handler=ATM.TemporaryThreat}
+s[586]   = {threat=ATM.Player.levelBasedThreat( -55, -3,  8, 18),handler=ATM.TemporaryThreat}
 
-    ["Shadow Word: Pain"] = {
-        ranks = {
-            10894,
-            10893,
-            10892,
-             2767,
-              992,
-              970,
-              594,
-              589,
-        },
-        threatMod = prototype.shadowAffinity,
-    },
+--Mind Blast
+s[10947] = {onDamage=true,threat=540,threatMod=prototype.shadowAffinity}
+s[10946] = {onDamage=true,threat=460,threatMod=prototype.shadowAffinity}
+s[10945] = {onDamage=true,threat=380,threatMod=prototype.shadowAffinity}
+s[8106]  = {onDamage=true,threat=303,threatMod=prototype.shadowAffinity}
+s[8105]  = {onDamage=true,threat=236,threatMod=prototype.shadowAffinity}
+s[8104]  = {onDamage=true,threat=180,threatMod=prototype.shadowAffinity}
+s[8103]  = {onDamage=true,threat=121,threatMod=prototype.shadowAffinity}
+s[8102]  = {onDamage=true,threat=77,threatMod=prototype.shadowAffinity}
+s[8092]  = {onDamage=true,threat=40,threatMod=prototype.shadowAffinity}
 
-    ["Vampiric Embrace"] = {
-        ranks = {
-            [15286] = 2, --CONFIRMED (TODO: generates 2 threat every proc)
-        },
-        threatMod = prototype.shadowAffinity,
-        type = "DEBUFF",
-    },
+--Mind Flay
+s[18807] = {onDebuff=true,threat=120,threatMod=prototype.shadowAffinity}
+s[17314] = {onDebuff=true,threat=104,threatMod=prototype.shadowAffinity}
+s[17313] = {onDebuff=true,threat=88,threatMod=prototype.shadowAffinity}
+s[17312] = {onDebuff=true,threat=72,threatMod=prototype.shadowAffinity}
+s[17311] = {onDebuff=true,threat=56,threatMod=prototype.shadowAffinity}
+s[15407] = {onDebuff=true,threat=40,threatMod=prototype.shadowAffinity}
 
-    ["Mind Control"] = {
-        ranks = {
-            10912,
-            10911,
-            605,
-        },
-        handler = ATM.MindControl,
-    },
+--Psychic Scream
+s[10890] = {isCC=true,onDebuff=true,threat=112}
+s[10888] = {isCC=true,onDebuff=true,threat=84}
+s[8124]  = {isCC=true,onDebuff=true,threat=56}
+s[8122]  = {isCC=true,onDebuff=true,threat=28}
+
+--Shadow Protection
+s[10958] = {onBuff=true,threat=56,threatMod=prototype.shadowAffinity}
+s[10957] = {onBuff=true,threat=42,threatMod=prototype.shadowAffinity}
+s[976]   = {onBuff=true,threat=30,threatMod=prototype.shadowAffinity}
+
+--Prayer of Shadow Protection
+s[27683] = {onBuff=true,threat=56,threatMod=prototype.shadowAffinity}
+
+--Shadow Word: Pain
+s[10894] = {onDebuff=true,threatMod=prototype.shadowAffinity}
+s[10893] = {onDebuff=true,threatMod=prototype.shadowAffinity}
+s[10892] = {onDebuff=true,threatMod=prototype.shadowAffinity}
+s[2767]  = {onDebuff=true,threatMod=prototype.shadowAffinity}
+s[992]   = {onDebuff=true,threatMod=prototype.shadowAffinity}
+s[970]   = {onDebuff=true,threatMod=prototype.shadowAffinity}
+s[594]   = {onDebuff=true,threatMod=prototype.shadowAffinity}
+s[589]   = {onDebuff=true,threatMod=prototype.shadowAffinity}
+
+--Vampiric Embrace
+s[15286] = {isLeech=true,onDebuff=true,onDamage=true,threat=2,threatMod=prototype.shadowAffinity}
+
+--Mind Control
+s[10912] = {handler=ATM.MindControl}
+s[10911] = {handler=ATM.MindControl}
+s[605]   = {handler=ATM.MindControl}
 
 
     --[[ Season of Discovery ]]--
-    ["Void Plague"] = {
-        ranks = {
-            [425204] = 2, --CONFIRMED
-        },
-        threatMod = prototype.shadowAffinity,
-        type = "DEBUFF",
-    },
+--Void Plague
+s[425204] = {onDebuff=true,threat=2,threatMod=prototype.shadowAffinity}
 
-    ["Homunculi"] = {
-        ranks = {
-            [402799] = 6, --CONFIRMED
-        },
-        threatMod = prototype.shadowAffinity,
-        type = "CAST",
-    },
-}
+--Homunculi
+s[402799] = {onCast=true,threat=6,threatMod=prototype.shadowAffinity}
